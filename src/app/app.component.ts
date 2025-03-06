9edae65795fca72fe5935cbd15536a5daa35203c
Юрий <avalanchedoto@mail.ru>
2025-03-06 23:52:46 +0300

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProfileCardComponent} from './common-ui/profile-card/profile-card.component';
 
---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,35 @@
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../auth/auth.service';
+import {from, map} from 'rxjs';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent {
+
+  authService = inject(AuthService)
+
+  form = new FormGroup({
+    username: new FormControl<string | null>(null, Validators.required),
+    password: new FormControl<string | null>(null, Validators.required)
+  })
+
+
+
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value)
+      //@ts-ignore
+      this.authService.login(this.form.value)
+        .subscribe()
+    }
+
+  }
+}
